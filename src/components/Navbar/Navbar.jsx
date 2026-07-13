f3280f42bb9ec63fb68aa1ba69bb765e132f71d6
Natani Guna Sri Durgapriya <priyanatani34073407@gmail.com>
2026-07-13 09:30:20 +0530

    updated navibar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,36 +2,24 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-
   return (
-
     <nav className="navbar">
-
       <h2>Placement Management System</h2>
-
       <div className="nav-links">
-
         <Link to="/dashboard">
           Dashboard
         </Link>
-
         <Link to="/student">
           Students
         </Link>
-
         <Link to="/register">
           Register
         </Link>
-
         <Link to="/login">
           Logout
         </Link>
-
       </div>
-
     </nav>
-
   );
 }
-
 export default Navbar;
