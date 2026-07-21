1cbd410c85f60c78188d3394f3b03eda76799a23
Guna Sri Durgapriya Natani <priyanatani34073407@gmail.com>
2026-07-21 20:21:48 +0530

    Refactor Navbar component structure and styles
    Updated Navbar component to use h1 and added inline styles.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,12 @@
-import "./Navbar.css";
-import { Link } from "react-router-dom";
-
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <h2>Placement Management System</h2>
-      <div className="nav-links">
-        <Link to="/dashboard">
-          Dashboard
-        </Link>
-        <Link to="/student">
-          Students
-        </Link>
-        <Link to="/register">
-          Register
-        </Link>
-        <Link to="/login">
-          Logout
-        </Link>
-      </div>
-    </nav>
+import './Navbar.css'
+function Navbar(){
+  return(
+    <nav className='navbar'>
+    <h1 style={{
+      color:"red",
+      // Tailwind css
+    }}>Placement Management System</h1>
+</nav>
   );
-}
+};
 export default Navbar;
