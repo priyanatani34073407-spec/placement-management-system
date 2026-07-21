2f389844070096e88ee69cf76a1d6116aba74fa7
Guna Sri Durgapriya Natani <priyanatani34073407@gmail.com>
2026-07-21 21:03:26 +0530

    Refactor Heading component for prop naming and syntax

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,13 +1,13 @@
-//My Second component
-//props:properties
-//ue de-structuring
-const Heading = function({name,Year}){
+// props : properties
+const Heading = function({name,year}){
   return(
-    //can write js in html
-    <div>
-      <h2>Welcome  {name}</h2>
-      <h2>Passing Year {Year}</h2>
-    </div>
+    // can write js in html
+    <>
+    
+    <h2>welcome {name}</h2>
+    <h2>Passing Year {year}</h2>
+    </>
   )
+
 };
-export default Heading
+export default Heading;
