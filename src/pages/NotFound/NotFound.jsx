832c28b9543d9080b3f2076899672a5ac9e1a1e1
Guna Sri Durgapriya Natani <priyanatani34073407@gmail.com>
2026-07-21 20:40:12 +0530

    Add NotFound component for 404 error page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound(){
+    return(
+        <div>
+            <h1>404</h1>
+            <h2>Page Bot Found</h2>
+            <p>The page you are looking for is not found</p>
+        </div>
+    )
+};
+export default NotFound;
