d20817a9d4306fae21524f41abfda600a5b483f5
Guna Sri Durgapriya Natani <priyanatani34073407@gmail.com>
2026-07-21 21:24:48 +0530

    Refactor import statement and improve comments
    Removed unnecessary space in import statement and updated comments for clarity.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  //Saftey to components by strict mode . 
-  //enable to detect changes in URL and not reload the page
+  // safety to components
+  // enables to detect changes in URL and 
   <BrowserRouter>
     <App />
   </BrowserRouter>
